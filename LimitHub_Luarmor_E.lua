eb0081dfbcd1227aea592e51a6c768256ac0f7a5
jorgethegreatq <annzkecastillo456@gmail.com>
2025-07-22 12:46:35 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_P_E.lua')))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/DBiceC6nMa/0", true))()
+loadstring(game:HttpGetAsync('https://pastefy.app/2Wygx4A3/raw'))()
